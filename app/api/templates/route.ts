bb997ecd595b62edea9b4c4ec521151834b52361
Tanujeet <singhtanujeet@gmail.com>
2025-07-25 09:59:19 +0530

    Added : Template delete  api

--- app/api/templates/route.ts
+++ app/api/templates/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const { userId } = await auth();
   if (!userId) {
     return new NextResponse("Unauthorised", { status: 401 });
@@ -55,5 +55,22 @@ export async function DELETE(req: Request) {
   const { userId } = await auth();
   if (!userId) {
     return new NextResponse("Unauthorised", { status: 401 });
+  }
+
+  try {
+    const { id } = await req.json();
+
+    if (!id) {
+      return new NextResponse("Template ID is required", { status: 400 });
+    }
+
+    const deletedTemplate = await prisma.template.deleteMany({
+      where: { userId },
+    });
+
+    return NextResponse.json(deletedTemplate);
+  } catch (e) {
+    console.error("Failed to delete template", e);
+    return new NextResponse("Internal server error", { status: 404 });
   }
 }
