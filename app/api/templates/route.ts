b1261297068228448c07f2c54851c5a0c7e33dca
Tanujeet <singhtanujeet@gmail.com>
2025-07-25 09:27:14 +0530

    Added : Api Structure

--- app/api/templates/route.ts
+++ app/api/templates/route.ts
@@ -1,3 +1,17 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorised", { status: 401 });
+  }
+
+  try {
+  } catch (e) {
+    console.error("Failed to fetch templates", e);
+  }
+}
+
 export async function POST(req: Request) {}
-export async function GET(req: Request) {}
 export async function DELETE(req: Request) {}
