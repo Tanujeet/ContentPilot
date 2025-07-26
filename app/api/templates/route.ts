1e2c1fb82093b131d1bf7932f0e60a1b424a6f12
Tanujeet <singhtanujeet@gmail.com>
2025-07-26 08:03:44 +0530

    Added : Template delete  api

--- app/api/templates/route.ts
+++ app/api/templates/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const { userId } = await auth();
