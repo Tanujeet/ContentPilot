8fec3fff38d33bbee9ea3bfae036205d8e969e2a
Tanujeet <singhtanujeet@gmail.com>
2025-07-29 09:31:08 +0530

    Added :Recent Activity

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,86 +14,98 @@ import {
 import { useEffect, useState } from "react";
 import { axiosInstance } from "@/lib/axios";
 import { error } from "console";
+type Activity = {
+  title: string;
+  status: string;
+  createdAt: string;
+};
 
 const Page = () => {
   const { user } = useUser();
   const username = user?.firstName || "User";
-const [stats, setStats] = useState({
-  totalPost: "...",
-  totalTemplates: "...",
-  totalGen: "...",
-});
-const statArray = [
-  { title: "Total Post", num: stats.totalPost },
-  { title: "Templates", num: stats.totalTemplates },
-  { title: "Generations", num: stats.totalGen },
-];
-const [recentActivity, setrecentActivity] = useState([]);
+  const [stats, setStats] = useState({
+    totalPost: "...",
+    totalTemplates: "...",
+    totalGen: "...",
+  });
+  const statArray = [
+    { title: "Total Post", num: stats.totalPost },
+    { title: "Templates", num: stats.totalTemplates },
+    { title: "Generations", num: stats.totalGen },
+  ];
 
-useEffect(() => {
-  const fetchStats = async () => {
-    try {
-      const res = await axiosInstance.get("/Stats");
-      setStats(res.data);
-    } catch (e) {
-      console.error("Failed to fetch stats:", e);
-    }
-  };
-  fetchStats();
-}, []);
-useEffect(() => {
-  const fetchActivity = async () => {
-    const res = await axiosInstance.get("/recent-activity");
-    setrecentActivity(res.data);
-  };
-  fetchActivity();
-}, []);
-return (
-  <main>
-    <section>
-      <div>
-        <h1 className="text-3xl font-bold ml-10 mt-10">
-          Welcome back , {username}
-        </h1>
-        <div className=" flex-grid grid grid-cols-3 gap-6 mt-10 ">
-          {statArray.map((stat, idx) => (
-            <Card key={idx} className="bg-[#1A1325]  text-white h-fit">
-              <CardHeader>
-                <CardTitle>{stat.title}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <h1 className="text-2xl">{stat.num}</h1>
-              </CardContent>
-            </Card>
-          ))}
+  const [recentActivity, setRecentActivity] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      try {
+        const res = await axiosInstance.get("/Stats");
+        setStats(res.data);
+      } catch (e) {
+        console.error("Failed to fetch stats:", e);
+      }
+    };
+    fetchStats();
+  }, []);
+  useEffect(() => {
+    const fetchActivity = async () => {
+      const res = await axiosInstance.get("/recent-activity");
+      setRecentActivity(res.data);
+    };
+    fetchActivity();
+  }, []);
+  return (
+    <main>
+      <section>
+        <div>
+          <h1 className="text-3xl font-bold ml-10 mt-10">
+            Welcome back , {username}
+          </h1>
+          <div className=" flex-grid grid grid-cols-3 gap-6 mt-10 ">
+            {statArray.map((stat, idx) => (
+              <Card key={idx} className="bg-[#1A1325]  text-white h-fit">
+                <CardHeader>
+                  <CardTitle>{stat.title}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <h1 className="text-2xl">{stat.num}</h1>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-    <section className="mt-20 ml-10 text-white">
-      <div>
-        <h1 className="text-3xl font-bold">Recent Activity</h1>
-        <Table className="mt-4 border border-black rounded-2xl ">
-          <TableCaption>A list of your recent invoices.</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Title</TableHead>
-              <TableHead>Status</TableHead>
+      </section>
+      <section className="mt-20 ml-10 text-white">
+        <div>
+          <h1 className="text-3xl font-bold">Recent Activity</h1>
+          <Table className="mt-4 border border-black rounded-2xl ">
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]  text-white">Title</TableHead>
+                <TableHead className=" text-white">Status</TableHead>
 
-              <TableHead className="text-right">Date</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className="font-medium">INV001</TableCell>
-              <TableCell>Paid</TableCell>
-              <TableCell>Credit Card</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
-    </section>
-  </main>
-);
+                <TableHead className="text-right  text-white">Date</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {recentActivity.map((activity, idx) => (
+                <TableRow key={idx}>
+                  <TableCell className="font-medium">
+                    {activity.title}
+                  </TableCell>
+                  <TableCell>{activity.status}</TableCell>
+                  <TableCell className="text-right">
+                    {activity.createdAt}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
