a2eb5be14ee241be515476f2e37cfe428585aee1
Tanujeet <singhtanujeet@gmail.com>
2025-07-29 10:07:39 +0530

    Added :Post page

---
+++ app/posts/page.tsx
@@ -0,0 +1,13 @@
+const Page = () => {
+  return (
+    <main>
+      <section>
+        <div>
+          <h1 className="text-4xl font-bold ml-10 mt-10">Post</h1>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Page;
