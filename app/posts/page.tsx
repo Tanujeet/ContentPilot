31dcb40d59cd8d63bd93952308bed7d33e643701
Tanujeet <singhtanujeet@gmail.com>
2025-07-31 20:35:10 +0530

    Added:Post page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,11 @@
+import { Input } from "@/components/ui/input";
 const Page = () => {
   return (
     <main>
       <section>
         <div>
           <h1 className="text-4xl font-bold ml-10 mt-10">Post</h1>
+          <Input />
         </div>
       </section>
     </main>
