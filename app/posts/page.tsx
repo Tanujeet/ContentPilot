512b0d08666d8399891870a9127e82d3d83677c2
Tanujeet <singhtanujeet@gmail.com>
2025-08-01 20:47:34 +0530

    Added:Card in post page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -22,7 +22,7 @@ const Page = () => {
                 <CardTitle>{contents.status}</CardTitle>
                 <CardTitle>{contents.Title}</CardTitle>
               </CardHeader>
-              hy there ypu are a
+              hy there ypu are a ass
               <CardContent>
                 <p>{contents.date}</p>
               </CardContent>
