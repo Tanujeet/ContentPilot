6b60a301ade34da7f7f5eeb78482b9ee6c7c422b
Tanujeet <singhtanujeet@gmail.com>
2025-08-04 20:16:05 +0530

    Added:Minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f9f9f9]`}
         >
