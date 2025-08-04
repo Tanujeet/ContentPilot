0e11925b1203bd3da933f256f21ebce7610d4b98
Tanujeet <singhtanujeet@gmail.com>
2025-08-04 20:34:32 +0530

    Added:minor changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,13 +23,8 @@ const Navbar = () => {
         </h1>
       </div>
       <SignedOut>
-        <SignInButton mode="modal">
-          <button className="bg-white text-black border border-gray-300 text-sm rounded-full px-4 py-2 hover:bg-gray-100 transition">
-            Sign In
-          </button>
-        </SignInButton>
         <SignUpButton mode="modal">
-          <button className="bg-black text-white rounded-full font-medium text-sm h-10 px-4 hover:bg-gray-800 transition">
+          <button className="bg-black text-white rounded-full font-medium text-sm h-10 px-4 hover:bg-gray-800 transition border border-white">
             Sign Up
           </button>
         </SignUpButton>
