c3021dd8f5e8e65ecda937b0c3380beae4b6d653
Tanujeet <singhtanujeet@gmail.com>
2025-08-06 09:49:22 +0530

    Added:changes in generate page

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,6 +1,13 @@
 const Page = () => {
   return (
-    <div>Page</div>
+    <main>
+      <section>
+        <div className="ml-10 mt-15">
+          <h1 className="text-4xl font-bold">Your idea</h1>
+          <div></div>
+        </div>
+      </section>
+    </main>
   );
 };
 
