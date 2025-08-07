3f40039086e768a9a3feb25c07a0a0935d22f804
Tanujeet <singhtanujeet@gmail.com>
2025-08-07 08:06:22 +0530

    Added:changes in generate page

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,10 +1,33 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const Page = () => {
   return (
     <main>
       <section>
         <div className="ml-10 mt-15">
           <h1 className="text-4xl font-bold">Your idea</h1>
-          <div></div>
+          <div className="mt-10">
+            <Select>
+              <h3>Tone</h3>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue
+                  placeholder="Professional"
+                  className="text-white"
+                />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="dark">Professional</SelectItem>
+                <SelectItem value="light">Friendly</SelectItem>
+                <SelectItem value="system">Casual</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
         </div>
       </section>
     </main>
